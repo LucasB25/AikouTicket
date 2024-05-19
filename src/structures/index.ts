8eab4eaee505c3010a8a6254cd8ad87e2d20dfb2
LucasB25#7323 <50886682+LucasB25@users.noreply.github.com>
2024-05-20 00:10:53 +0200

    update

--- src/structures/index.ts
+++ src/structures/index.ts
@@ -2,6 +2,7 @@ import { Events } from 'discord.js';
 
 import Bot from './Client.js';
 import Command from './Command.js';
+import Context from './Context.js';
 import Event from './Event.js';
 
-export { Bot, Event, Command, Events };
+export { Bot, Event, Command, Events, Context };
